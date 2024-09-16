d3f34f9f83f5e624c6f9006df0bbf6899a6077ac
Mohamad Moukayed <hyper1creeper@gmail.com>
2024-09-16 09:23:44 +0400

    changed debug to release

--- studentsunion/settings.py
+++ studentsunion/settings.py
@@ -128,11 +128,11 @@ EMAIL_USE_TLS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "http://localhost:81/static/"
+STATIC_URL = "https://su.amb.sch.ae:81/static/"
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = 'http://localhost:81/media/'
+MEDIA_URL = 'http://su.amb.sch.ae:81/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
