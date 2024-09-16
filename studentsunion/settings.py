d0c928622fe25f437ffb80db69d32ce65d7cfdfd
Mohamad Moukayed <hyper1creeper@gmail.com>
2024-09-16 09:37:04 +0400

    changed debug to release 2

--- studentsunion/settings.py
+++ studentsunion/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = "https://su.amb.sch.ae:81/static/"
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = 'http://su.amb.sch.ae:81/media/'
+MEDIA_URL = 'https://su.amb.sch.ae:81/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
